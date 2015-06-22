b1ee0ff90af93224d21ecdea8038882f1a618fab
noprom <tyee.noprom@qq.com>
2015-06-22 21:56:46 +0800

    Initial Commit

---
+++ ObjectCourse/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectCourse
+//
+//  Created by noprom on 15/6/22.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
